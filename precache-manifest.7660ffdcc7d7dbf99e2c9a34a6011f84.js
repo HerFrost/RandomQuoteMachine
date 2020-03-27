0e04531ae7b2c8372ad6d452c98b1abbf8507067
HerFrost <vidojevicvlada@gmail.com>
2020-03-27 20:17:29 +0100

    Updates

---
+++ precache-manifest.7660ffdcc7d7dbf99e2c9a34a6011f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7021868f2d267c16ae2b10f6ee52ff52",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "a535a939c3a413c5d88b",
+    "url": "/RandomQuoteMachine/static/css/main.aa4d42ed.chunk.css"
+  },
+  {
+    "revision": "f1bce1f630a5ebfb56f8",
+    "url": "/RandomQuoteMachine/static/js/2.b7dd03d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuoteMachine/static/js/2.b7dd03d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a535a939c3a413c5d88b",
+    "url": "/RandomQuoteMachine/static/js/main.48ade823.chunk.js"
+  },
+  {
+    "revision": "c858e17a27537d09b3c2",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.87885edf.js"
+  }
+]);
